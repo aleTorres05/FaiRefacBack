7591942333ff11111b2e798ff7597f7684072d64
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-09-29 08:08:06 -0600

    [FEAT]Add createQuoteVersionByRepairShop useCase and router

--- src/usecases/mechanic.usecase.js
+++ src/usecases/mechanic.usecase.js
@@ -1,17 +1,17 @@
-const createEror = require('http-errors');
+const createError = require('http-errors');
 const Mechanic = require('../models/mechanic.model');
 
 async function create(mechanicData) {
     const { firstName, lastName, workshopName, phoneNumber, address } = mechanicData;
 
     if (!firstName || !lastName || !workshopName || !phoneNumber || !address) {
-        throw createEror(400, "All fields are required");
+        throw createError(400, "All fields are required");
     }
 
     const existingMechanic = await Mechanic.findOne({ phoneNumber });
 
     if (existingMechanic) {
-        throw createEror(409, " A mechanic with this phone number already exist");
+        throw createError(409, " A mechanic with this phone number already exist");
     }
 
     const newMechanic = await Mechanic.create(mechanicData);
