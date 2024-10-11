01c90173cb8c8e2e9a7c56b2db384c017dc35023
Alejandro Torres <torresalexatlanta@gmail.com>
2024-10-11 16:40:22 -0600

    added endpoint to getByEmail for user

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,118 +1,154 @@
-const express = require('express');
-const userUseCase = require('../usecases/user.usecase');
-const auth = require('../middlewares/auth.middleware');
-const upload = require('../middlewares/upload.middleware');
-
+const express = require("express");
+const userUseCase = require("../usecases/user.usecase");
+const auth = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/upload.middleware");
 
 const router = express.Router();
 
-router.get('/:id', auth, async (req,res) => {
-    const { id } =req.params;
-    try {
-        const user = await userUseCase.getById(id);
-        res.json({
-            success: true,
-            data: { user },
-        });
-    } catch (error) {
-        res.status(error.status || 500);
-        res.json({
-            success: false,
-            error: error.message,
-        });
-    }
-})
-
-router.post('/', async (req, res) => {
-    try {
-        const createdUser = await userUseCase.create(req.body);
-        res.json({
-            success: true,
-            data: { User: createdUser},
-        })
-    } catch (error) {
-        res.status(error.status || 500);
-        res.json({
-            success: false,
-            error: error.message,
-        });
-    };
+router.get("/:email", auth, async (req, res) => {
+  const { email } = req.params;
+  try {
+    const user = await userUseCase.getByEmail(email);
+    res.json({
+      success: true,
+      data: { user },
+    });
+  } catch (error) {
+    res.status(error.status || 500);
+    res.json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-router.patch('/:id/client', auth, upload.single('profilePicture'), async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userUseCase.getById(id);
+    res.json({
+      success: true,
+      data: { user },
+    });
+  } catch (error) {
+    res.status(error.status || 500);
+    res.json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const createdUser = await userUseCase.create(req.body);
+    res.json({
+      success: true,
+      data: { User: createdUser },
+    });
+  } catch (error) {
+    res.status(error.status || 500);
+    res.json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+router.patch(
+  "/:id/client",
+  auth,
+  upload.single("profilePicture"),
+  async (req, res) => {
     const { id } = req.params;
     const userId = req.user._id;
-    const file = req.file
-    
-    try {
-        const updatedUser = await userUseCase.updateByIdUserClient(id, req.body, userId, file);
-        res.json({
-            success: true,
-            data: { user: updatedUser },
-        });
-    } catch (error) {
-        res.status(error.status || 500);
-        res.json({
-            success: false,
-            error: error.message,
-        });
-    }
-});
+    const file = req.file;
 
-router.patch('/:id/repairShop', auth, upload.single('profilePicture'), async (req, res) => {
+    try {
+      const updatedUser = await userUseCase.updateByIdUserClient(
+        id,
+        req.body,
+        userId,
+        file
+      );
+      res.json({
+        success: true,
+        data: { user: updatedUser },
+      });
+    } catch (error) {
+      res.status(error.status || 500);
+      res.json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+);
+
+router.patch(
+  "/:id/repairShop",
+  auth,
+  upload.single("profilePicture"),
+  async (req, res) => {
     const { id } = req.params;
     const userId = req.user._id;
-    const file = req.file
-    
+    const file = req.file;
+
     try {
-        const updatedUser = await userUseCase.updateByIdUserRepairShop(id, req.body, userId, file);
-        res.json({
-            success: true,
-            data: { user: updatedUser },
-        });
+      const updatedUser = await userUseCase.updateByIdUserRepairShop(
+        id,
+        req.body,
+        userId,
+        file
+      );
+      res.json({
+        success: true,
+        data: { user: updatedUser },
+      });
     } catch (error) {
-        res.status(error.status || 500);
-        res.json({
-            success: false,
-            error: error.message,
-        });
+      res.status(error.status || 500);
+      res.json({
+        success: false,
+        error: error.message,
+      });
     }
+  }
+);
+
+router.post("/send-otp", auth, async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const sendOTP = await userUseCase.generateAndSendOTP(email);
+    res.json({
+      success: true,
+      data: { message: "OTP was sended successfuly." },
+    });
+  } catch (error) {
+    res.status(error.status || 500);
+    res.json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-router.post('/send-otp', auth, async (req, res) => {
-    const { email } = req.body
+router.post("/verify-otp", auth, async (req, res) => {
+  const { email, otp } = req.body;
 
-    try {
-        const sendOTP = await userUseCase.generateAndSendOTP(email);
-        res.json({
-            success: true,
-            data: { message: 'OTP was sended successfuly.'}
-        });
-    } catch (error) {
-        res.status(error.status || 500);
-        res.json({
-            success: false,
-            error: error.message,
-        });
-    }
-});
-
-router.post('/verify-otp', auth, async (req, res) => {
-    const {email, otp } = req.body;
-
-    try {
-        const verifiedOTP = await userUseCase.verifyOTP(email, otp);
-        res.json({
-            success: true,
-            data: { message: 'User was successfyly verified.'}
-        })
-    } catch (error) {
-        res.status(error.status || 500);
-        res.json({
-            success: false,
-            error: error.message,
-        });
-    }
+  try {
+    const verifiedOTP = await userUseCase.verifyOTP(email, otp);
+    res.json({
+      success: true,
+      data: { message: "User was successfyly verified." },
+    });
+  } catch (error) {
+    res.status(error.status || 500);
+    res.json({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
